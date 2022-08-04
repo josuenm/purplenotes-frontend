a6c7a92f89aab24cd319ce19bbc2e9b2f91dff45
josuenm <direct.josue@gmail.com>
2022-08-04 17:06:46 -0300

    Contextos

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import RouteController from "./RouteController";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { Login } from "./pages/Login";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalToolsContextProvider } from "./contexts/globalToolsContext";
+import { UserContextProvider } from "./contexts/userContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <div className="App">
+  <GlobalToolsContextProvider>
+    <UserContextProvider>
       <BrowserRouter>
         <RouteController />
       </BrowserRouter>
-    </div>
-  </React.StrictMode>
+    </UserContextProvider>
+  </GlobalToolsContextProvider>
 );
