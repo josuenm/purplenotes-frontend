fc1277f4c54ef4cfb2bd45f7e8b873bbe1809e55
josuenm <direct.josue@gmail.com>
2022-08-04 16:03:29 -0300

    Estrutura inicial com compnoente de RouteController

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import RouteController from "./RouteController";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App";
+import { Login } from "./pages/Login";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <BrowserRouter>
+        <RouteController />
+      </BrowserRouter>
+    </div>
   </React.StrictMode>
 );
