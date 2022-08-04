3ded0eab2f7808e584b348b53e01b68f432dbad5
josuenm <direct.josue@gmail.com>
2022-08-04 18:30:35 -0300

    Funções para lidar com user, salvar e prover

--- src/contexts/userContext.jsx
+++ src/contexts/userContext.jsx
@@ -1,9 +1,65 @@
-import React, { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { GlobalToolsContext } from "./globalToolsContext";
+import { UserServices } from "../services/axios/users";
+import { useNavigate } from "react-router-dom";
+import nookies from "nookies";
 
-const UserContext = createContext(null);
+export const UserContext = createContext(null);
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const { handleLoading } = useContext(GlobalToolsContext);
 
-  return <UserContext.Provider value={{}}>{children}</UserContext.Provider>;
+  const navigation = useNavigate();
+
+  function handleUser({ name, email }) {
+    localStorage.setItem("jsnotes.user", JSON.stringify({ name, email }));
+  }
+
+  async function Login(data) {
+    handleLoading(true);
+    const response = await UserServices.login(data);
+
+    switch (response.status) {
+      case 200:
+        nookies.set(null, "jsnotes.token", response.data.token);
+        handleUser({
+          name: response.data.user.name,
+          email: response.data.user.email,
+        });
+        navigation("/dashboard");
+        break;
+    }
+
+    handleLoading(false);
+  }
+
+  async function Register(data) {
+    handleLoading(true);
+    const response = await UserServices.register(data);
+
+    switch (response.status) {
+      case 201:
+        nookies.set(null, "jsnotes.token", response.data.token);
+        handleUser({
+          name: response.data.user.name,
+          email: response.data.user.email,
+        });
+        navigation("/dashboard");
+        break;
+    }
+
+    handleLoading(false);
+  }
+
+  useEffect(() => {
+    const userLocalStorage = JSON.parse(localStorage.getItem("jsnotes.user"));
+    setUser(userLocalStorage);
+  }, []);
+
+  return (
+    <UserContext.Provider value={{ Login, Register, user }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
