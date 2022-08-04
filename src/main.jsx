3efebefa8a17d1860df9628a2af10448a21034cf
josuenm <direct.josue@gmail.com>
2022-08-04 18:29:01 -0300

    Provider do contexto de notas

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,16 @@ import { Login } from "./pages/Login";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalToolsContextProvider } from "./contexts/globalToolsContext";
 import { UserContextProvider } from "./contexts/userContext";
+import { NotesContextProvider } from "./contexts/notesContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GlobalToolsContextProvider>
-    <UserContextProvider>
-      <BrowserRouter>
-        <RouteController />
-      </BrowserRouter>
-    </UserContextProvider>
-  </GlobalToolsContextProvider>
+  <BrowserRouter>
+    <GlobalToolsContextProvider>
+      <UserContextProvider>
+        <NotesContextProvider>
+          <RouteController />
+        </NotesContextProvider>
+      </UserContextProvider>
+    </GlobalToolsContextProvider>
+  </BrowserRouter>
 );
