80d5617d48c0c4e533b58068260de8fc7025c26a
josuenm <direct.josue@gmail.com>
2022-08-05 20:17:41 -0300

    Código mais limpo, oragnizado e usando componentes styled-compnoents

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,79 +1,69 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
   background: var(--purple-100);
-  height: 100vh;
+  min-height: 100vh;
 
-  @media (max-width: 575.98px) {
-    height: auto;
-  }
-  @media (min-width: 575.99px) and (max-width: 991.98px) {
-    height: 100vh;
-  }
+  .Container {
+    min-height: 100vh;
 
-  .home__body {
     display: flex;
-    justify-content: center;
-    padding: 0 5rem;
-    padding-top: 7rem;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2rem;
 
     @media (max-width: 991.98px) {
       flex-direction: column;
-      align-items: center;
+      gap: 4rem;
       justify-content: flex-start;
-      margin-top: 3rem;
       padding: 2rem 1rem;
     }
+  }
+`;
 
-    .home__body__title {
-      display: flex;
-      flex-direction: column;
+export const InfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  color: #fff;
+`;
 
-      color: #fff;
+export const ButtonContainer = styled.div`
+  margin-top: 2rem;
 
-      h2 {
-        font-size: 2.5rem;
-        margin: 0;
-      }
+  a {
+    padding: 0.8rem 1rem;
+    font-size: 1.2rem;
+    font-weight: 600;
+    border-radius: 0.25rem;
+    border: 1px solid #fff;
 
-      p {
-        font-size: 1.2rem;
-      }
-
-      .home__body__titleButton {
-        margin-top: 2rem;
-
-        a {
-          padding: 1rem;
-          font-size: 1.2rem;
-          font-weight: 600;
-          border-radius: 0.25rem;
-          border: 1px solid #fff;
-
-          transition: all 0.2s;
-          &:hover {
-            background: #fff;
-            color: var(--purple-100);
-          }
-        }
-      }
-    }
-
-    .home__body__presentation {
-      @media (max-width: 991.98px) {
-        margin-top: 4rem;
-      }
-
-      img {
-        transform: translateY(4.2rem);
-        width: 100%;
-
-        @media (max-width: 991.98px) {
-          transform: translateX(-1rem);
-          width: 100%;
-        }
-      }
+    transition: all 0.2s;
+    &:hover {
+      background: #fff;
+      color: var(--purple-100);
     }
   }
+`;
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  margin: 0;
+`;
+
+export const Description = styled.h2`
+  font-weight: 400;
+  font-size: 1.2rem;
+`;
+
+export const PresentationContainer = styled.div`
+  min-height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export const PresentationImage = styled.img`
+  max-width: 2000px;
+  width: 100%;
 `;
