eab5cc0c4eb44dfa5cbc58df466b62f0de833b59
josuenm <direct.josue@gmail.com>
2022-08-05 20:11:12 -0300

    Lógica para redirecionamento feita

--- src/RouteController.jsx
+++ src/RouteController.jsx
@@ -1,30 +1,46 @@
 import { useNavigate, Route, Routes, useLocation } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Dashboard } from "./pages/Dashboard";
+import { EditNote } from "./pages/EditNote";
 import { Header } from "./components/Header";
 import { Register } from "./pages/Register";
 import { Login } from "./pages/Login";
 import { UserEdit } from "./pages/UserEdit";
+import { NoteList } from "./pages/NoteList";
 import { HeaderLogged } from "./components/HeaderLogged";
 import { useContext, useEffect } from "react";
 import { UserContext } from "./contexts/userContext";
 
 export default function RouteController() {
-  const pathname = useLocation();
+  const { pathname } = useLocation();
   const navigate = useNavigate();
 
   const { user } = useContext(UserContext);
 
+  const redirectionRoutesForUsers = ["/", "/register", "/login"];
+
+  useEffect(() => {
+    const isRedirect = redirectionRoutesForUsers.some(
+      (route) => pathname === route
+    );
+
+    if (user) {
+      if (isRedirect) {
+        navigate("/dashboard");
+        return;
+      }
+    } else if (!isRedirect) {
+      navigate("/");
+    }
+  }, [user]);
+
   return (
-    <>
-      {!!user ? <HeaderLogged /> : <Header />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/user/edit" element={<UserEdit />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<NoteList />} />
+      <Route path="/dashboard/:id/edit" element={<EditNote />} />
+      <Route path="/dashboard/user/edit" element={<UserEdit />} />
+    </Routes>
   );
 }
