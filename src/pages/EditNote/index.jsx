2aa0987f3dd3dc894df6cd2bbfac5da9bfa536c3
josuenm <direct.josue@gmail.com>
2022-08-05 20:07:48 -0300

    Página de editar anotação

---
+++ src/pages/EditNote/index.jsx
@@ -0,0 +1,25 @@
+import { Container } from "./styles";
+import { Sidebar } from "../../components/Sidebar";
+import { SafeZone } from "../../components/SafeZone";
+import { NotesServices } from "../../services/axios/notes";
+import { Editor } from "../../components/Editor";
+import { useCallback, useContext, useEffect } from "react";
+import { GlobalToolsContext } from "../../contexts/globalToolsContext";
+import { NotesContext } from "../../contexts/notesContext";
+
+export function EditNote() {
+  const { sidebarIsOpen } = useContext(GlobalToolsContext);
+  const { update } = useContext(NotesContext);
+
+  const updateNote = async (oldNote, params) => {
+    const updatedNote = await update(oldNote.id, params);
+  };
+
+  return (
+    <Container>
+      <SafeZone>
+        <Editor updateNote={updateNote} />
+      </SafeZone>
+    </Container>
+  );
+}
