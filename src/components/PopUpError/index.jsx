9a6e9f915db1ae392cf51d792f761ea010966220
josuenm <direct.josue@gmail.com>
2022-08-06 17:58:04 -0300

    Animação com framer-motion

--- src/components/PopUpError/index.jsx
+++ src/components/PopUpError/index.jsx
@@ -1,7 +1,36 @@
 import { Container } from "./styles";
+import { motion, AnimatePresence } from "framer-motion";
 
-const PopUpError = ({ title }) => {
-  return <Container>{title}</Container>;
+const PopUpError = ({ error }) => {
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={!!error}
+        style={{
+          position: "fixed",
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: "999",
+        }}
+        initial={{
+          bottom: -200,
+          opacity: 0,
+        }}
+        animate={
+          !!error
+            ? {
+                bottom: 70,
+                opacity: 1,
+              }
+            : { bottom: -200, opacity: 0, display: "none" }
+        }
+        exit={{ bottom: -200, opacity: 0 }}
+        transition={{ duration: 0.4 }}
+      >
+        <Container>{error}</Container>
+      </motion.div>
+    </AnimatePresence>
+  );
 };
 
 export { PopUpError };
