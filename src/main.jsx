ce3b3bbde5f128cc4d4d099182b27f702dc66199
josuenm <direct.josue@gmail.com>
2022-08-06 18:21:48 -0300

    Componente de estilo global

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import ReactDOM from "react-dom/client";
 import RouteController from "./RouteController";
-import "./index.css";
 import { Login } from "./pages/Login";
 import { BrowserRouter } from "react-router-dom";
 import { GlobalToolsContextProvider } from "./contexts/globalToolsContext";
 import { UserContextProvider } from "./contexts/userContext";
 import { NotesContextProvider } from "./contexts/notesContext";
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <GlobalStyles />
     <GlobalToolsContextProvider>
       <UserContextProvider>
         <NotesContextProvider>
