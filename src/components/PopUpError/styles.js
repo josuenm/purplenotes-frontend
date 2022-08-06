8e4f1fcae01b7f6f374586e03f706045fdf7f363
josuenm <direct.josue@gmail.com>
2022-08-06 17:58:19 -0300

    Estilos apagados

--- src/components/PopUpError/styles.js
+++ src/components/PopUpError/styles.js
@@ -1,16 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  position: absolute;
-  z-index: 999;
-  left: 50%;
-  transform: translateX(-50%);
-
   width: 400px;
-
   text-align: center;
   border-radius: 0.25rem;
   padding: 0.5rem 1.5rem;
   background-color: #f75959;
   color: #fff;
+  font-weight: 500;
 `;
