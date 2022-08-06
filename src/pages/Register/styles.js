54e605210faf35b68f591a7d17d883948f665a33
josuenm <direct.josue@gmail.com>
2022-08-05 22:46:33 -0300

    Tirando background

--- src/pages/Register/styles.js
+++ src/pages/Register/styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  background: var(--purple-100);
   min-height: 100vh;
   overflow: hidden;
   padding-top: 3rem;
