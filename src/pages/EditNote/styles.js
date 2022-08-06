51fb6155a4999bd2ea6929cb176b2a7adf567b95
josuenm <direct.josue@gmail.com>
2022-08-05 22:51:09 -0300

    Estilização do componente Container e WarningText

--- src/pages/EditNote/styles.js
+++ src/pages/EditNote/styles.js
@@ -1,5 +1,14 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  margin-top: 3rem;
+`;
+
+export const WarningText = styled.p`
+  color: #aaa;
+  font-size: 1.5rem;
+  text-align: center;
 `;
