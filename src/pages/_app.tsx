717b61e42f176e70e9a1e076cd75323c83e4f8b0
josuenm <direct.josue@gmail.com>
2022-08-24 14:28:12 -0300

    minW and minH has been added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import type { AppProps } from "next/app";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Box as="main" bgColor="#eee">
+      <Box as="main" bgColor="#eee" minH="100vh" minW="100vw">
         <Component {...pageProps} />
       </Box>
     </ChakraProvider>
