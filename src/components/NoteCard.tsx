1396e6fa2d5c253898c774cc726829866fddfd02
josuenm <direct.josue@gmail.com>
2022-08-24 15:33:40 -0300

    Components folder with NoteCard component

---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,44 @@
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { NoteProps } from "../types/NoteProps";
+
+interface NoteCardProps {
+  note: NoteProps;
+}
+
+const NoteCard = ({ note }: NoteCardProps) => {
+  const formatedTitle = (title: string) => {
+    if (title.length >= 20) {
+      return title.substring(0, 20) + "...";
+    }
+    return title;
+  };
+
+  const formatedBody = (body: string) => {
+    let newBody = body.replace(/<(.|\n)*?>/gi, "").slice(0, 20);
+
+    if (newBody.length >= 20) {
+      return newBody.substring(0, 20) + "...";
+    }
+    return newBody;
+  };
+
+  return (
+    <Box
+      w={["full", "full", 80]}
+      borderWidth={1}
+      borderColor="gray.400"
+      p={5}
+      borderRadius="2xl"
+    >
+      <Heading fontSize={21}>{formatedTitle(note.title)}</Heading>
+      <Text>{formatedBody(note.body)}</Text>
+
+      <Flex mt={5} direction="column" gap={2}>
+        <Button colorScheme="violet">Access</Button>
+        <Button colorScheme="red">Delete</Button>
+      </Flex>
+    </Box>
+  );
+};
+
+export default NoteCard;
