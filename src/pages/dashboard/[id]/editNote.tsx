2d3bdd5a384da099a604ab6cdbe4ef25d61531c4
josuenm <direct.josue@gmail.com>
2022-08-25 19:52:50 -0300

    Function to update note and Head with title

--- src/pages/dashboard/[id]/editNote.tsx
+++ src/pages/dashboard/[id]/editNote.tsx
@@ -1,7 +1,9 @@
 import { Center, Container, Heading } from "@chakra-ui/react";
-import { NextPage } from "next";
+import { NotesContext } from "@contexts/NotesContext";
+import { GetServerSideProps, NextPage } from "next";
 import dynamic from "next/dynamic";
-import { useState } from "react";
+import Router, { useRouter } from "next/router";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 import "react-quill/dist/quill.snow.css";
 import HeaderWithBackButton from "src/components/HeaderWithBackButton";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
@@ -25,11 +27,66 @@ const formats = [
   "link",
 ];
 
+import Head from "next/head";
+import { parseCookies } from "nookies";
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { "purplenotes.token": token } = parseCookies(ctx);
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
+
 const Dashboard: NextPage = () => {
   const [value, setValue] = useState("");
 
+  const { UpdateNote, notes } = useContext(NotesContext);
+  const { query } = useRouter();
+
+  const findById = useCallback(() => {
+    const currentNote = notes.find((note) => note._id === query.id);
+
+    if (!currentNote) {
+      Router.push("/dashboard");
+      return;
+    }
+    setValue(currentNote.body);
+  }, [notes, query.id]);
+
+  const timeout = useRef<NodeJS.Timeout>();
+
+  function updateNote(value: string) {
+    setValue(value);
+
+    clearTimeout(timeout.current);
+    timeout.current = setTimeout(() => {
+      UpdateNote(query.id as string, {
+        title: value.replace(/<(.|\n)*?>/gi, "").slice(0, 20),
+        body: value,
+      });
+    }, 2000);
+  }
+
+  useEffect(() => {
+    findById();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
+      <Head>
+        <title>Edit Note | Purple Notes</title>
+      </Head>
       <HeaderWithBackButton returnTo="/dashboard" />
       <Container>
         <Center
@@ -40,13 +97,13 @@ const Dashboard: NextPage = () => {
           mx="auto"
           minH="100vh"
         >
-          <Heading fontSize={21} color="gray.600">
+          <Heading fontSize={21} color="gray.600" textAlign="center">
             The note is automatically saved
           </Heading>
           <ReactQuill
             theme="snow"
             value={value}
-            onChange={setValue}
+            onChange={updateNote}
             modules={modules}
             formats={formats}
             style={{ width: "100%" }}
