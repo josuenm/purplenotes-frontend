7554cd72170bf3bd2319b359a08e41fb55d8e628
josuenm <direct.josue@gmail.com>
2022-08-25 00:11:43 -0300

    Provider do contexto GlobalTools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
+import { GlobalToolsProvider } from "@contexts/GlobalTools";
 import theme from "@utils/theme";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Box as="main" bgColor="#eee" minH="100vh" minW="full">
-        <Component {...pageProps} />
-      </Box>
+      <GlobalToolsProvider>
+        <Box as="main" bgColor="#eee" minH="100vh" minW="full">
+          <Component {...pageProps} />
+        </Box>
+      </GlobalToolsProvider>
     </ChakraProvider>
   );
 }
