40de517ec1b4bba26a4da87f5a5cba1c17e44f15
josuenm <direct.josue@gmail.com>
2022-08-25 19:58:11 -0300

    Env config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    BACK_END_URL: process.env.BACK_END_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
