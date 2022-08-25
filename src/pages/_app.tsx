ce7e33098e468f5a0a414450353dbb0eb13da85e
josuenm <direct.josue@gmail.com>
2022-08-25 16:07:30 -0300

    Context providers added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
-import { GlobalToolsProvider } from "@contexts/GlobalTools";
+import { GlobalToolsContextProvider } from "@contexts/GlobalToolsContext";
+import { NotesContextProvider } from "@contexts/NotesContext";
+import { UserContextProvider } from "@contexts/UserContext";
 import theme from "@utils/theme";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <GlobalToolsProvider>
-        <Box as="main" bgColor="#eee" minH="100vh" minW="full">
-          <Component {...pageProps} />
-        </Box>
-      </GlobalToolsProvider>
+      <GlobalToolsContextProvider>
+        <UserContextProvider>
+          <NotesContextProvider>
+            <Box as="main" bgColor="#eee" minH="100vh" minW="full">
+              <Component {...pageProps} />
+            </Box>
+          </NotesContextProvider>
+        </UserContextProvider>
+      </GlobalToolsContextProvider>
     </ChakraProvider>
   );
 }
